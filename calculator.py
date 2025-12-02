3f0fde815096ffcb7315db2cd1eb6a8d71809db7
pabitramaharjan199-oss <pabitra.maharjan199@gmail.com>
2025-12-02 19:57:49 +0545

    added calculator functionalities

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# Simple Calculator
+
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operator.")
